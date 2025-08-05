1d1f603a4791b64e306fcb56cb1cc94d98ee2b96
Oskar Paulsson <oskar.paulsson@afry.com>
2025-08-05 15:43:21 +0200

    add syslog

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <syslog.h>
 
 int main(int argc, char* argv[]) {
+	openlog(NULL, 0, LOG_USER);
+	syslog(LOG_INFO, "writer application started");
 	if (argc != 3) {
-		printf("Usage: %s <arg1> <arg2>\n", argv[0]);
-		exit(1);
+		syslog(LOG_ERR, "Invalid number of arguments! Number of given arguments: %d. Usage: %d <arg1> <arg2>\n", argc, argv[0]);
+		return 1;
 	}
 
 	FILE* file_ptr;
 	file_ptr = fopen(argv[1], "w");
 	if (file_ptr == NULL) {
-		printf("Error occured while opening file");
-		exit(1);
+		syslog(LOG_ERR, "Error occured while opening file");
+		return 1;
 	}
 
 	fputs(argv[2], file_ptr);
